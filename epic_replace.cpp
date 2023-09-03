cae67551123f7433fc0b9baf6d07469ca1577357
le-me <133717528+le-me@users.noreply.github.com>
2023-09-03 00:45:19 -0400

    Create epic_replace.cpp

---
+++ epic_replace.cpp
@@ -0,0 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+#define STRING_EMPTY _strdup("")
+
+typedef unsigned int uint;
+
+uint clamp(uint in, uint min, uint max)
+{
+	if (in < min) return min;
+	if (in > max) return max;
+	return in;
+}
+
+char* strsub(char* source, int startPos, int endPos)
+{
+	int length = clamp((endPos - startPos) + 1, 0, strlen(source));
+
+	char* newString = new char[length];
+	memcpy(newString, &source[startPos], length);
+	newString[length - 1] = '\0';
+
+	return newString;
+}
+
+uint strcount(char* sourceString, char* oldString)
+{
+	uint sourceLength = strlen(sourceString);
+	uint oldLength = strlen(oldString);
+
+	uint count = 0;
+
+	char oldFirst = oldString[0];
+	for (int i = 0; i < sourceLength; i++)
+	{
+		if (sourceString[i] != oldFirst) continue;
+
+		char* test = strsub(sourceString, i, i + oldLength);
+		if (strcmp(test, oldString) == 0)
+			count++;
+	}
+
+	return count;
+}
+
+char* strappend(char* string, char character)
+{
+	uint length = strlen(string);
+
+	char* newString = new char[length + 2];
+	strcpy(newString, string);
+
+	newString[length] = character;
+	newString[length + 1] = '\0';
+
+	return newString;
+}
+
+char* strreplace(char* sourceString, char* oldString, char* newString)
+{
+	uint sourceLength = strlen(sourceString);
+	uint oldLength = strlen(oldString);
+	uint newLength = strlen(newString);
+
+	uint occurences = strcount(sourceString, oldString);
+	int lengthDifference = abs((int)oldLength - (int)newLength);
+	int lengthOffset = ((lengthDifference * occurences) + 1);
+
+	uint finalLength = 0;
+	if (newLength < oldLength)
+		finalLength = sourceLength - lengthOffset;
+	else
+		finalLength = sourceLength + lengthOffset;
+
+	char* finalString = new char[1];
+	finalString[0] = '\0';
+
+	char oldFirst = oldString[0];
+	for (int i = 0; i < sourceLength; i++)
+	{
+		if (sourceString[i] != oldFirst)
+		{
+			finalString = strappend(finalString, sourceString[i]);
+			continue;
+		}
+
+		char* test = strsub(sourceString, i, i + oldLength);
+		if (strcmp(test, oldString) == 0)
+		{
+			strcat(finalString, newString);
+			i += oldLength - 1;
+		}
+		else
+			finalString = strappend(finalString, sourceString[i]);
+	}
+
+	return finalString;
+}
+
+char* strreplace(const char* sourceString, const char* oldString, const char* newString)
+{
+	return strreplace(_strdup(sourceString), _strdup(oldString), _strdup(newString));
+}
+
+int main()
+{
+	const char* test = "Hello my name is erratic!";
+	std::cout << "Initial Result: " << test << "\n";
+
+	char* newTest = strreplace(test, "erratic", "leme");
+	std::cout << "Final Result: " << newTest << "\n\n";
+
+	return 0;
+}
